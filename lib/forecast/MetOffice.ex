7c36e79c621fe8f9705b39f4fcb7b453b523bdd1
Paul Wilson <paulanthonywilson@gmail.com>
2014-01-31 14:42:29 +0000

    wip

--- lib/forecast/MetOffice.ex
+++ lib/forecast/MetOffice.ex
@@ -1,21 +1,32 @@
 defmodule Forecast.MetOffice do
   defmodule ApiData do
     alias HTTPotion.Response
+    @api_key "f016d482-0238-42e0-919d-c580163410b3"
 
     @user_agent  [ "User-agent": "Elixir:"]
 
-    def fetch(url) do
+
+    def fetch(url_portion, params) do
+      url = "http://datapoint.metoffice.gov.uk/public/data/val/#{url_portion}?#{param_string(params)}"
       case HTTPotion.get(url, @user_agent) do
         Response[body: body, status_code: status, headers: _headers ] when status in 200..299 -> { :ok, body }
         Response[body: body, status_code: _status, headers: _headers ] -> { :error, body }
       end
     end
+
+    defp param_string(params) do
+      (params ++ [key: @api_key]
+        |> Enum.map(fn {key, val} -> "#{key}=#{val}" end))
+        |> Enum.join("&")
+    end
   end
-  @api_key "f016d482-0238-42e0-919d-c580163410b3"
+
+  defmodule Decode do
+
+  end
 
   def site_list do
-    "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?res=daily&key=#{@api_key}"
-      |> ApiData.fetch
+    ApiData.fetch "wxfcs/all/json/sitelist", [res: "daily"]
   end
 end
 
--- test/met_office_test.exs
+++ test/met_office_test.exs
@@ -1,17 +1,63 @@
 defmodule MetOfficeDataTest do
   use ExUnit.Case
   alias HTTPotion.Response
+  import Forecast.MetOffice.ApiData, only: [fetch: 2]
 
   setup do
     :meck.new(HTTPotion)
     :ok
   end
 
+  teardown do
+    :meck.unload(HTTPotion)
+    :ok
+  end
 
-  test "successful status returns ok with the body" do
-    :meck.expect(HTTPotion, :get, fn _url, _agent -> Response[status_code: 200, body: "everything is fine"] end
-    )
+  defp stub_httpotion_get(f) do
+    :meck.expect(HTTPotion, :get, f)
+  end
 
-    assert Forecast.MetOffice.ApiData.fetch("hi") == {:ok, "everything is fine"}
+
+  test "successful request returns ok with the body" do
+    stub_httpotion_get fn _url, _agent -> Response[status_code: 200, body: "everything is fine"] end
+
+    assert Forecast.MetOffice.ApiData.fetch("hi", []) == {:ok, "everything is fine"}
+  end
+
+  test "unnsuccessful request returns error with the body" do
+    stub_httpotion_get fn _url, _agent -> Response[status_code: 500, body: "everything is terrible"] end
+    assert Forecast.MetOffice.ApiData.fetch("hi", []) == {:error, "everything is terrible"}
+  end
+
+
+  test "the metoffice url is used with the api key" do
+    stub_httpotion_get fn url, _ ->
+      assert url == "http://datapoint.metoffice.gov.uk/public/data/val/abc/def?key=f016d482-0238-42e0-919d-c580163410b3"
+      Response.new
+    end
+
+    fetch "abc/def", []
+
+  end
+
+  test "url with extra parameter" do
+    stub_httpotion_get fn url, _ ->
+      assert url == "http://datapoint.metoffice.gov.uk/public/data/val/abc/def?res=daily&key=f016d482-0238-42e0-919d-c580163410b3"
+      Response.new
+    end
+
+    fetch "abc/def", [res: "daily"]
+  end
+
+end
+
+
+defmodule ApiDecodeTest do
+  use ExUnit.Case
+  import ForecastMetOffice.Decode
+
+  test "decodes the Json" do
+    assert (File.read!("#{Path.dirname(__FILE__)}/locations_fixture.json") |> decode_site_list) == "hello"
   end
 end
+
