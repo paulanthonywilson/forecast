299db72407aa00ff05d685476dd33177f799a6e6
Paul Wilson <paulanthonywilson@gmail.com>
2014-02-03 23:09:01 +0000

    provide a non-exception throwing mechanism for met office errors. Unsure about this

--- lib/forecast.ex
+++ lib/forecast.ex
@@ -6,4 +6,12 @@ defmodule Forecast do
   def start(_type, _args) do
     Forecast.Supervisor.start_link
   end
+
+  def nearest_sites(latlon, count) do
+    try do
+    {:ok, Forecast.MetOffice.nearest_sites(latlon, count)}
+    rescue
+      e in [RuntimeError] -> {:error, e.message}
+    end
+  end
 end
--- test/forecast_test.exs
+++ test/forecast_test.exs
@@ -1,7 +1,32 @@
 defmodule ForecastTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  setup do
+    :meck.new(Forecast.MetOffice)
+    :ok
+  end
+
+  teardown do
+    :meck.unload(Forecast.MetOffice)
+    :ok
+  end
+
+  test "returns ok without exception" do
+    :meck.expect(Forecast.MetOffice, :nearest_sites, fn latlon, count->
+      assert latlon == {55.6, -3.1}
+      assert count == 4
+      "sites!"
+    end);
+
+    assert Forecast.nearest_sites({55.6, -3.1}, 4) == {:ok, "sites!"}
+  end
+
+  test "returns error when there's an exception" do
+    :meck.expect(Forecast.MetOffice, :nearest_sites, fn _latlon, _count->
+      raise "Oh dear!"
+    end);
+
+    assert Forecast.nearest_sites({55.6, -3.1}, 4) == {:error, "Oh dear!"}
+
   end
 end
